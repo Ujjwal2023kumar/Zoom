a924d04d942036a6afac7bed0581b6f383a2cf46
Ujjwal Kumar <164533721+Ujjwal2023kumar@users.noreply.github.com>
2025-08-07 08:58:46 +0530

    Update app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,11 +1,8 @@
 import express from "express";
 import { createServer } from "node:http";
-
 import { Server } from "socket.io";
-
 import mongoose from "mongoose";
 import { connectToSocket } from "./controllers/socketManager.js";
-
 import cors from "cors";
 import userRoutes from "./routes/users.routes.js";
 
@@ -18,22 +15,14 @@ app.set("port", (process.env.PORT || 8000))
 app.use(cors());
 app.use(express.json({ limit: "40kb" }));
 app.use(express.urlencoded({ limit: "40kb", extended: true }));
-
 app.use("/api/v1/users", userRoutes);
 
 const start = async () => {
     app.set("mongo_user")
-    const connectionDb = await mongoose.connect("mongodb+srv://imdigitalashish:imdigitalashish@cluster0.cujabk4.mongodb.net/")
-
+    const connectionDb = await mongoose.connect("")
     console.log(`MONGO Connected DB HOst: ${connectionDb.connection.host}`)
     server.listen(app.get("port"), () => {
         console.log("LISTENIN ON PORT 8000")
     });
-
-
-
 }
-
-
-
 start();
