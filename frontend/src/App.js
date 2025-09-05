573d32225e938fd08ccaf02b13f0ca8e31c600a8
Ujjwal Kumar <164533721+Ujjwal2023kumar@users.noreply.github.com>
2025-09-06 00:48:47 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,3 @@
-import './App.css';
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import LandingPage from './pages/landing';
 import Authentication from './pages/authentication';
@@ -10,27 +9,18 @@ import History from './pages/history';
 function App() {
   return (
     <div className="App">
-
       <Router>
-
         <AuthProvider>
-
-
           <Routes>
-
             <Route path='/' element={<LandingPage />} />
-
             <Route path='/auth' element={<Authentication />} />
-
             <Route path='/home's element={<HomeComponent />} />
             <Route path='/history' element={<History />} />
             <Route path='/:url' element={<VideoMeetComponent />} />
           </Routes>
         </AuthProvider>
-
       </Router>
     </div>
   );
 }
-
 export default App;
